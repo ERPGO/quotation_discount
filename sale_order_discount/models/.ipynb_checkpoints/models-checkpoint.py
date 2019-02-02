a10145e6ed2257710e0be80e2a4227c727b655dc
nurlanf <18286626+nurlanf@users.noreply.github.com>
2019-02-02 14:19:17 +0000

    add_discount

---
+++ sale_order_discount/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class sale_order_discount(models.Model):
+#     _name = 'sale_order_discount.sale_order_discount'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+
+class sale_order_discount(models.Model):
+    _inherit = 'sale.order'
+    
+    discount_method = fields.Selection([('fixed', 'Fixed'), ('percentage', 'Percentage')])
+    discount_amount = fields.Float(string='Discount amount', default="0.0")
+    total_discount = fields.Float(string='Discount')
+    
+    @api.depends('discount_method'):
+        def _get_discount(self):
+            total_discount = 0.0
+            if self.discount_method == 'fixed':
+                total_discount = self.discount_amount
+            elif self.discount_method = 'percentage':
+                total_discount = self.amount_total * self.discount_amount / 100
+            return total_discount
+        
+    
+    @api.depends('order_line.price_total')
+    def _amount_all(self):
+        """
+        Compute the total discount of the SO.
+        """
+        for order in self:
+            amount_untaxed = amount_tax = total_discount 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            order.update({
+                'total_discount': _get_discount(),
+                'amount_untaxed': amount_untaxed,
+                'amount_tax': amount_tax,
+                'amount_total': amount_untaxed + amount_tax - _get_discount(),
+            })
+
+    
