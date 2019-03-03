be30e1cc0cced6c40a05a344d3f6e21bb75e22bf
nurlanf <18286626+nurlanf@users.noreply.github.com>
2019-03-03 11:58:31 +0000

    NEWBORN

--- sale_discount_total/__manifest__.py
+++ sale_discount_total/__manifest__.py
@@ -1,33 +1,12 @@
 # -*- coding: utf-8 -*-
-###################################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2017-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: fasluca(<https://www.cybrosys.com>)
-#
-#    This program is free software: you can modify
-#    it under the terms of the GNU Affero General Public License (AGPL) as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-###################################################################################
-
 {
     'name': 'Sale Discount on Total Amount',
     'version': '11.0.1.0.1',
     'category': 'Sales Management',
     'summary': "Discount on Total in Sale and Invoice With Discount Limit and Approval",
-    'author': 'Cybrosys Techno Solutions',
-    'company': 'Cybrosys Techno Solutions',
-    'website': 'http://www.cybrosys.com',
+    'author': 'ERPGO.AZ',
+    'company': 'ERPGO.AZ',
+    'website': 'http://www.erpgo.az',
     'description': """
 
 Sale Discount for Total Amount
@@ -48,7 +27,7 @@ Module to manage discount on total amount in Sale.
     ],
     'demo': [
     ],
-    'images': ['static/description/banner.jpg'],
+    #'images': ['static/description/banner.jpg'],
     'license': 'AGPL-3',
     'application': True,
     'installable': True,
