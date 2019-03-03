aa6611cf1f02b6fff3f0bc661852b254ef1851e4
nurlanf <nurlan.farajov@gmail.com>
2019-03-03 10:30:13 +0100

    Update

--- sale_discount_total/models/account_invoice.py
+++ sale_discount_total/models/account_invoice.py
@@ -29,11 +29,12 @@ class AccountInvoice(models.Model):
     @api.one
     @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'tax_line_ids.amount_rounding',
                  'currency_id', 'company_id', 'date_invoice', 'type')
-    def _compute_amount(self):
+    def _compute_amount( self ):
         self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
         self.amount_tax = sum(line.amount_total for line in self.tax_line_ids)
         self.amount_total = self.amount_untaxed + self.amount_tax
-        self.amount_discount = sum((line.quantity * line.price_unit * line.discount)/100 for line in self.invoice_line_ids)
+        self.amount_discount = sum(
+            (line.quantity * line.price_unit * line.discount) / 100 for line in self.invoice_line_ids)
         amount_total_company_signed = self.amount_total
         amount_untaxed_signed = self.amount_untaxed
         if self.currency_id and self.currency_id != self.company_id.currency_id:
@@ -47,12 +48,13 @@ class AccountInvoice(models.Model):
 
     discount_type = fields.Selection([('percent', 'Percentage'), ('amount', 'Amount')], string='Discount Type',
                                      readonly=True, states={'draft': [('readonly', False)]}, default='percent')
-    discount_rate = fields.Float('Discount Amount', digits=(16, 2), readonly=True, states={'draft': [('readonly', False)]})
+    discount_rate = fields.Float('Discount Amount', digits=(16, 2), readonly=True,
+                                 states={'draft': [('readonly', False)]})
     amount_discount = fields.Monetary(string='Discount', store=True, readonly=True, compute='_compute_amount',
                                       track_visibility='always')
 
     @api.onchange('discount_type', 'discount_rate', 'invoice_line_ids')
-    def supply_rate(self):
+    def supply_rate( self ):
         for inv in self:
             if inv.discount_type == 'percent':
                 for line in inv.invoice_line_ids:
@@ -69,12 +71,13 @@ class AccountInvoice(models.Model):
                     line.discount = discount
 
     @api.multi
-    def compute_invoice_totals(self, company_currency, invoice_move_lines):
+    def compute_invoice_totals( self, company_currency, invoice_move_lines ):
         total = 0
         total_currency = 0
         for line in invoice_move_lines:
             if self.currency_id != company_currency:
-                currency = self.currency_id.with_context(date=self.date or self.date_invoice or fields.Date.context_today(self))
+                currency = self.currency_id.with_context(
+                    date=self.date or self.date_invoice or fields.Date.context_today(self))
                 line['currency_id'] = currency.id
                 line['amount_currency'] = currency.round(line['price'])
                 line['price'] = currency.compute(line['price'], company_currency)
@@ -92,9 +95,10 @@ class AccountInvoice(models.Model):
         return total, total_currency, invoice_move_lines
 
     @api.multi
-    def button_dummy(self):
+    def button_dummy( self ):
         self.supply_rate()
         return True
+
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
